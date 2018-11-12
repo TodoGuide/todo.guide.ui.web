5919f7eaaa919761dc4921f543a4e78eecd207ae
James Tharpe <james@brainbytescreative.com>
2018-11-12 13:05:51 -0500

    Auto schedules events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Calendar from "react-big-calendar";
+import BigCalendar from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
@@ -7,40 +7,34 @@ import "./App.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import logo from "./logo.svg";
+import events from "./Data/events"
 
-const localizer = Calendar.momentLocalizer(moment) // or globalizeLocalizer
-const DnDCalendar = withDragAndDrop(Calendar);
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+const Calendar = withDragAndDrop(BigCalendar);
 
 class App extends Component {
   state = {
-    events: [
-      {
-        start: new Date(),
-        end: new Date(moment().add(1, "hours")),
-        title: "Scaffold the calendar"
-      },
-      {
-        start: new Date(moment().add(1, "hours")),
-        end: new Date(moment().add(30, "minutes")),
-        title: "Do other stuff"
-      }
-    ]
+    events: events.schedule()
   };
 
-  onEventResize = (type, { event, start, end, allDay }) => {
+  onEventResize = ({ event, start, end}) => {
     this.setState(state => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: state.events };
+      console.log(event);
+      event.duration = moment.duration(moment(end).diff(moment(start))).asMinutes();
+      event.priority = start;
+      event.start = start;
+      event.end = end;
+      return { events: state.events.schedule() };
     });
   };
 
   onEventDrop = ({ event, start, end, allDay }) => {
     console.log(start);
     this.setState(state => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: state.events };
+      event.priority = start;
+      event.start = start;
+      event.end = end;
+      return { events: state.events.schedule() };
     });
   };
 
@@ -54,7 +48,7 @@ class App extends Component {
         <p className="App-intro">
           Give me your todo list, I schedule it for you
         </p>
-        <DnDCalendar
+        <Calendar
           defaultDate={new Date()}
           defaultView="day"
           events={this.state.events}
--- src/Data/events.js
+++ src/Data/events.js
@@ -1,105 +1,47 @@
-const events = [{
-  id: 0,
-  title: 'All Day Event very long title',
-  allDay: true,
-  start: new Date(2018, 10, 0),
-  end: new Date(2018, 10, 1),
-},
-{
-  id: 1,
-  title: 'Long Event',
-  start: new Date(2018, 10, 7),
-  end: new Date(2018, 10, 10),
-},
+import moment from "moment";
 
-{
-  id: 2,
-  title: 'DTS STARTS',
-  start: new Date(2016, 2, 13, 0, 0, 0),
-  end: new Date(2016, 2, 20, 0, 0, 0),
-},
+const events = [
+  {
+    priority: 1,
+    duration: 60,
+    title: "First Priority"
+  },
+  {
+    priority: 5,
+    duration: 90,
+    title: "Fifth Priority"
+  },
+  {
+    priority: 3,
+    duration: 120,
+    title: "Third Priority"
+  },
+];
 
-{
-  id: 3,
-  title: 'DTS ENDS',
-  start: new Date(2016, 10, 6, 0, 0, 0),
-  end: new Date(2016, 10, 13, 0, 0, 0),
-},
+function setStart(event, start){
+  const startMoment = moment(start);
+  event.start = startMoment.toDate();
+  event.end = startMoment.add(event.duration, "minutes").toDate();
+}
 
-{
-  id: 4,
-  title: 'Some Event',
-  start: new Date(2018, 10, 9, 0, 0, 0),
-  end: new Date(2018, 10, 10, 0, 0, 0),
-},
-{
-  id: 5,
-  title: 'Conference',
-  start: new Date(2018, 10, 11),
-  end: new Date(2018, 10, 13),
-  desc: 'Big conference for important people',
-},
-{
-  id: 6,
-  title: 'Meeting',
-  start: new Date(2018, 10, 12, 10, 30, 0, 0),
-  end: new Date(2018, 10, 12, 12, 30, 0, 0),
-  desc: 'Pre-meeting meeting, to prepare for the meeting',
-},
-{
-  id: 7,
-  title: 'Lunch',
-  start: new Date(2018, 10, 12, 12, 0, 0, 0),
-  end: new Date(2018, 10, 12, 13, 0, 0, 0),
-  desc: 'Power lunch',
-},
-{
-  id: 8,
-  title: 'Meeting',
-  start: new Date(2018, 10, 12, 14, 0, 0, 0),
-  end: new Date(2018, 10, 12, 15, 0, 0, 0),
-},
-{
-  id: 9,
-  title: 'Happy Hour',
-  start: new Date(2018, 10, 12, 17, 0, 0, 0),
-  end: new Date(2018, 10, 12, 17, 30, 0, 0),
-  desc: 'Most important meal of the day',
-},
-{
-  id: 10,
-  title: 'Dinner',
-  start: new Date(2018, 10, 12, 20, 0, 0, 0),
-  end: new Date(2018, 10, 12, 21, 0, 0, 0),
-},
-{
-  id: 11,
-  title: 'Birthday Party',
-  start: new Date(2018, 10, 13, 7, 0, 0),
-  end: new Date(2018, 10, 13, 10, 30, 0),
-},
-{
-  id: 12,
-  title: 'Late Night Event',
-  start: new Date(2018, 10, 17, 19, 30, 0),
-  end: new Date(2018, 10, 18, 2, 0, 0),
-},
-{
-  id: 12.5,
-  title: 'Late Same Night Event',
-  start: new Date(2018, 10, 17, 19, 30, 0),
-  end: new Date(2018, 10, 17, 23, 30, 0),
-},
-{
-  id: 13,
-  title: 'Multi-day Event',
-  start: new Date(2018, 10, 20, 19, 30, 0),
-  end: new Date(2018, 10, 22, 2, 0, 0),
-},
-{
-  id: 14,
-  title: 'Today',
-  start: new Date(new Date().setHours(new Date().getHours() - 3)),
-  end: new Date(new Date().setHours(new Date().getHours() + 3)),
-},]
+events.schedule = function(){
+  if(this.length === 0) return this;
+  this.sort(function(a, b){
+    return a.priority - b.priority;
+  });
+  setStart(this[0], Date.now());
+  const lastIndex = this.length -1;
+  for(let i = 0; i <= lastIndex; i++){
+    const current = this[i];
+    if(i < lastIndex){
+      const next = this[i + 1];
+      setStart(next, current.end);
+    }
+    current.priority = current.start;
+  }
+  console.log(this)
+  return this;
+}
+
+
 export default events;
