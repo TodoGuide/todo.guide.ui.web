611b41bcf71fbfded1d8fab13233cb2fe093900d
James Tharpe <james@brainbytescreative.com>
2018-11-12 16:33:55 -0500

    Moves todos to a class

--- src/Data/events.js
+++ src/Data/events.js
@@ -1,43 +1,38 @@
 import moment from "moment";
+import Todo from "../Todo/Todo";
 
 const events = [
-  {
+  new Todo({
+    title: "Ability to mark an item as complete",
     priority: 10,
     duration: 120,
-    title: "Ability to mark an item as complete"
-  },
-  {
+  }),
+  new Todo({
+    title: "Persist data offline",
     priority: 15,
     duration: 180,
-    title: "Persist data offline"
-  },
-  {
+  }),
+  new Todo({
+    title: "Host on GitHub static sites",
     priority: 20,
     duration: 90,
-    title: "Host on GitHub static sites"
-  },
+  }),
 ];
-
-function setStart(event, start){
-  const startMoment = moment(start);
-  event.start = startMoment.toDate();
-  event.end = startMoment.add(event.duration, "minutes").toDate();
-}
 
 events.schedule = function(){
   if(this.length === 0) return this;
   this.sort(function(a, b){
-    return a.priority - b.priority;
+    return a.start - b.start;
   });
-  setStart(this[0], Date.now());
+  this[0].start = new Date();
   const lastIndex = this.length -1;
   for(let i = 0; i <= lastIndex; i++){
     const current = this[i];
     if(i < lastIndex){
       const next = this[i + 1];
-      setStart(next, current.end);
+      next.start = current.end;
     }
-    current.priority = current.start;
+    current.start = current.start;
   }
   console.log(this)
   return this;
