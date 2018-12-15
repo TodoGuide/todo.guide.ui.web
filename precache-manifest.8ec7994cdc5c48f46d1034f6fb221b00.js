9ad089e4ccb897d8863306cbecd203cec5d320ab
James Tharpe <james@brainbytescreative.com>
2018-12-15 16:33:48 -0500

    Publishing to gh-pages as of Sat, Dec 15, 2018  4:33:48 PM

---
+++ precache-manifest.8ec7994cdc5c48f46d1034f6fb221b00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo.guide.ui.web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d05241a7a18d21c5f79",
+    "url": "/todo.guide.ui.web/static/js/runtime~main.5d05241a.js"
+  },
+  {
+    "revision": "5320661a2ecb0bedc21d",
+    "url": "/todo.guide.ui.web/static/js/main.5320661a.chunk.js"
+  },
+  {
+    "revision": "4585b7977e15b8c69523",
+    "url": "/todo.guide.ui.web/static/js/1.4585b797.chunk.js"
+  },
+  {
+    "revision": "5320661a2ecb0bedc21d",
+    "url": "/todo.guide.ui.web/static/css/main.32bca115.chunk.css"
+  },
+  {
+    "revision": "4585b7977e15b8c69523",
+    "url": "/todo.guide.ui.web/static/css/1.f515ce7f.chunk.css"
+  },
+  {
+    "revision": "2f8eaa8163c02e59df4fd192dea98090",
+    "url": "/todo.guide.ui.web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.875ef6408f825fc952cfd52a1acfcd08.js"
+  "/todo.guide.ui.web/precache-manifest.8ec7994cdc5c48f46d1034f6fb221b00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo.guide.ui.web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
